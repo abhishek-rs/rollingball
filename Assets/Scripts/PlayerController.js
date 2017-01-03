eae9142e1d972c0a1b6aad46c91432ee42dce520
Abhishek Shetty <abhishek10.rs@gmail.com>
2017-01-03 21:20:19 +0200

    built on android, added game end

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -1,21 +1,69 @@
 ﻿#pragma strict
 
-    public var speed : float;
+import UnityEngine.UI;
 
+    public var speed : float;
+    public var countText : Text;
     public var rb : Rigidbody;
+    private var count : int;
+    public var win : Text;
+    public var zeroAc: Vector3;
+    public var curAc: Vector3;
+    public var sensH: float = 10;
+    public var sensV: float = 10;
+    public var smooth: float = 0.5;
+    public var GetAxisH: float = 0;
+    public var GetAxisV: float = 0;
 
    	function Start ()
     {
+   		ResetAxes();
         rb = GetComponent.<Rigidbody>();
+        count = 0;
+        setCountText();
+        win.text = "";
+
     }
+
+    function ResetAxes(){
+    	zeroAc = Input.acceleration;
+    	curAc = Vector3.zero;
+    }
+
 
  	function FixedUpdate ()
     {
         var moveHorizontal : float = Input.GetAxis("Horizontal");
         var moveVertical : float = Input.GetAxis("Vertical");
 
-        var movement : Vector3 = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        curAc = Vector3.Lerp(curAc, Input.acceleration-zeroAc, Time.deltaTime/smooth);
+        GetAxisV = Mathf.Clamp(curAc.y * sensV, -1, 1);
+        GetAxisH = Mathf.Clamp(curAc.x * sensH, -1, 1);
+
+        var movement : Vector3 = new Vector3 (GetAxisH, 0.0f, GetAxisV);
 
         rb.AddForce (movement * speed);
     }
 
+    function OnTriggerEnter (other : Collider) {
+		if( other.gameObject.CompareTag ("PickUp")){
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			setCountText();
+		}
+
+	}
+
+	function setCountText(){
+		countText.text = "Count: " + count.ToString();
+		if (count >= 23) {
+			win.text = "You've won you brilliant piece of shit!";
+		}
+	}
+
+
+
+
+
+	
+	
---
+++ Assets/Scripts/Rotator.js
@@ -0,0 +1,5 @@
+﻿#pragma strict
+
+function Update () {
+	transform.Rotate (new Vector3 (14,30,45) * Time.deltaTime);
+}
