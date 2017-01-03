cc733753bba1c2a6d8573a60830e3005516eb8ac
Abhishek Shetty <abhishek10.rs@gmail.com>
2017-01-03 17:56:52 +0200

    first commit, ball movement using keys

---
+++ Assets/Scripts/PlayerController.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+    public var speed : float;
+
+    public var rb : Rigidbody;
+
+   	function Start ()
+    {
+        rb = GetComponent.<Rigidbody>();
+    }
+
+ 	function FixedUpdate ()
+    {
+        var moveHorizontal : float = Input.GetAxis("Horizontal");
+        var moveVertical : float = Input.GetAxis("Vertical");
+
+        var movement : Vector3 = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
